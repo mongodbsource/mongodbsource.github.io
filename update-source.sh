acf4304cd76d910a3a8a39c380921104a5c98450
Louis Williams <louis.williams@mongodb.com>
2018-06-18 14:23:44 -0400

    Update script

--- update-source.sh
+++ update-source.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 SRC_BASE=${SRC_BASE:-src}
-SRC_REPO=${SRC_REPO:-git@github.com:mongodb/mongo}
+SRC_REPO=${SRC_REPO:-git@github.com:louiswilliams/mongo}
 SRC_BRANCH=${SRC_BRANCH:-master}
+GIT_PUSH=${GIT_PUSH:-false}
 
 set -e 
 if [ ! -d "$SRC_BASE" ]; then
@@ -10,11 +11,24 @@ if [ ! -d "$SRC_BASE" ]; then
     git clone $SRC_REPO $SRC_BASE
 fi
 
+GIT_ROOT=$(pwd)
+
 cd $SRC_BASE
+git pull
 git checkout $SRC_BRANCH
 cd docs
 doxygen storage.Doxyfile
-cd ..
+
+cd $GIT_ROOT
 
 rm -rf $SRC_BRANCH
+echo "in `pwd`"
 cp -R $SRC_BASE/docs/html $SRC_BRANCH
+
+if [ "$GIT_PUSH" = "true" ]; then
+    git add $SRC_BRANCH
+    git commit -m "Updating source for $(date). This is an automatic commit"
+    git push
+else
+    echo "skipping push"
+fi
